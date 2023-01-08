688249ea50f99c74af6df21741f0293a0656e892
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-01-08 10:55:48 +0530

    meme-updated

--- src/Pages/Memes/MemeGenerator.jsx
+++ src/Pages/Memes/MemeGenerator.jsx
@@ -4,27 +4,19 @@ import { useQuery } from 'react-query';
 const randomNumber = Math.floor(Math.random() * 3);
 
 const MemeGenerator = () => {
-
-    // const [count, setCount] = useState(randomNumber)
-
     const MemeApi = async () => {
         const res =  await fetch('https://meme-api.com/gimme');
         return res.json()
     }
-
+    
     const {status, data} = useQuery('meme', MemeApi)
-
+    
     if(status === 'loading'){
         return <h1>...LOADING</h1>
     }
-
+    
     if(status === 'error'){
         return <h1>ERROR</h1>
-    }
-
-    const newMemeClick = (e) => {
-        e.preventDefault()
-        window.location.reload()
     }
 
   return (
@@ -34,9 +26,6 @@ const MemeGenerator = () => {
             
             <img className='meme-img' src={data.preview[randomNumber]} alt="meme" />
 
-            <button className="cool-btn" onClick={newMemeClick}>
-                🔂 
-            </button>
         </section>
     </main>
   )
