d7244c8f3b1b01fa90671715a5b720ac6a923c7c
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-01-10 09:58:08 +0530

    todo-added

--- src/Pages/Todo/Todo.jsx
+++ src/Pages/Todo/Todo.jsx
@@ -1,16 +1,60 @@
-import React from 'react'
-import TodoInput from './TodoInput'
-import TodoItem from './TodoItem'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Todo = () => {
+
+  const localData = localStorage.getItem('TASK_ITEM_LOCAL')
+  const [task, setTask] = useState(localData ? JSON.parse(localData) : [])
+  const [isChecked, setIsChecked] = useState(false)
+  const todo = useRef()
+  
+  const addTodo = (e) => {
+    if(todo.current.value === '') return null
+    // console.log(todo.current.value);
+    // task = todo.current.value
+    // localStorage.setItem('localTask', task)
+    setTask([...task, todo.current.value])
+    console.log(task);
+    todo.current.value = ''
+  }
+
+  const handleDeleteAll = () => {
+    localStorage.clear()
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    localStorage.setItem('TASK_ITEM_LOCAL', JSON.stringify(task))
+  }, [task])
+
+  // const localItem = JSON.parse(localStorage.getItem('localTask'))
+
   return (
     <main>
-        <h1 style={{color: '#ffffff', textAlign: 'center'}}>Whimsicle 🌪 </h1>  
-        <section className="intro-section">
-            <sub>Time To Be Productive!</sub>
-            <TodoInput />
-            <TodoItem task={'Task'} />
-        </section>
+      <h1 style={{color: '#ffffff', textAlign: 'center'}}>Whimsicle 🌪 </h1>  
+      <section className="intro-section">
+        <h1>Go ultra productive, comrade!</h1>
+        <div className="input-form">
+          <input type="text" name="task" id="task"
+            placeholder='Enter your tak'
+            ref={todo}
+          />
+          <button className='wow-btn' onClick={addTodo}>
+            ➕ 
+          </button>
+          <button className="wow-btn" onClick={handleDeleteAll}>
+            🗑 
+          </button>
+        </div>
+
+      <ol>
+        {
+          task.map((item, index) => {
+            return <li className='list-txt todo-item' key={index}>{item}
+            </li>
+          })
+        }
+      </ol>
+      </section>
     </main>
   )
 }
